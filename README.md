d21a57a6c690827dd5f62331b3d75b4f262a0238
Cristobal Ortega <cristo@us.ibm.com>
2015-11-17 04:02:36 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Daily Programmer Reddit Challenges   
+==  
+
+Challenges from https://www.reddit.com/r/dailyprogrammer/
---
+++ unicodeChess/chess.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (int argc, char* argv[]) {
+
+	fprintf(stdout,"\u265c\n");
+	return 0;
+}
