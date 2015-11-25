c4c0e460bf1c042b246e9826a64830452fe3dc05
Cristobal Ortega <cristo@us.ibm.com>
2015-11-25 09:55:51 -0500

    first commit 242.vhs

---
+++ 242.vhs/README.md
@@ -0,0 +1,3 @@
+Challenge #242
+
+from https://www.reddit.com/r/dailyprogrammer/comments/3u6o56/20151118_challenge_242_intermediate_vhs_recording/
---
+++ 242.vhs/vhs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//backtracking function
+int recordedShows(vector nextShows){
+	forall nextshows
+		result = recordedshows(nextshows[i])
+		if result > actual.result
+			actual.result == result
+}
+
+int main (int argc, char* argv[]) {
+
+	printf("Enter schedule: ");
+	//read schedule into vector/map
+	//order by starting time
+	//backtracking:
+	//	-until nextShows empty
+	//		-pick one possible show (start time next >= end time actual)
+	//	-return shows recorded
+	return 0;
+}
